00e7fc6132184dd10a76d5f99de338866d1e2ea9
riteshsutariya <riteshsutariya94@gmail.com>
2024-06-28 18:08:16 +0530

    deletion in binary tree

--- python/binary-tree.py
+++ python/binary-tree.py
@@ -45,7 +45,6 @@ def insert(root, value):
     
     while q:
         temp = q.popleft()
-        print('temp:- ',temp.item)
         if temp.left is None:
             temp.left = Node(value)
             break
@@ -59,31 +58,87 @@ def insert(root, value):
             q.append(temp.right)
     return root
 
+def delete_deepest(root, target_node):
+    q = []
+    q.append(root)
+    while(len(q)):
+        temp = q.pop(0)
+        if temp == target_node:
+            temp = None
+            del temp
+            return
+        
+        if temp.left:
+            if temp.left == target_node:
+                temp.left = None
+                del target_node
+                return
+            else:
+                q.append(temp.left)
+        
+        if temp.right:
+            if temp.right == target_node:
+                temp.right = None
+                del target_node
+                return
+            else:
+                q.append(temp.right)
+        
 def delete(root, value):
     if root is None:
         return None
     
     if root.left is None and root.right is None:
-        if root.value == value:
+        if root.item == value:
             return None
         else:
             return root
-        
+    
+    q = deque()
+    q.append(root)
+    
+    temp = None
+    target_node = None
 
+    while q:
+        temp = q.popleft()
+        if temp.item == value:
+            target_node = temp
+        if temp.left is not None:
+            q.append(temp.left)
+        if temp.right is not None:
+            q.append(temp.right)
+
+    if target_node is not None:
+        target_node.item = temp.item
+        delete_deepest(root, temp)
+        print('targer found')
+    else:
+        print('target not found!')
+    return root
 root = None
 
-values = [25,50,75,15,20,29,35,45,95,28,82,92,88,91,97]
+# values = [25,50,75,15,20,29,35,45,95,28,82,92,88,91,97]
+values = [25,50,75]
 
 for value in values:
     root = insert(root,value)
 
 print("Inorder traversal ")
 inorder(root)
-
-print("\nPreorder traversal ")
-preorder(root)
-
-print("\nPostorder traversal ")
-postorder(root)
-
 print()
+
+root = delete(root,75)
+
+print("Inorder traversal ")
+inorder(root)
+print()
+
+# print("\nPreorder traversal ")
+# preorder(root)
+
+# print("\nPostorder traversal ")
+# postorder(root)
+
+# print()
+
--- python/revision/binary_tree.py
+++ python/revision/binary_tree.py
@@ -1,22 +1,12 @@
+
+from collections import deque
+
 class Node:
     def __init__(self, key):
         self.left = None
         self.right = None
         self.value = key
         
-root = Node(10)
-root.left = Node(12)
-root.left.left = Node(20)
-root.left.right = Node(24)
-root.left.left.left = Node(38)
-root.left.left.right = Node(40)
-
-root.right = Node(18)
-root.right.left = Node(28)
-root.right.right = Node(32)
-root.right.left.left = Node(45)
-root.right.right.right = Node(50)
-
 def inorder(root):
     if root is not None:
         inorder(root.left)
@@ -44,12 +34,54 @@ def postorder(root):
         # print('not a valid tree')
         return None
     
-def insert(root,value):
-    pass
+def insert(root, value):
+    # tree not having any node
+    if root is None:
+        return Node(value)
+    
+    # tree with root only
+    if root.left is None and root.right is None:
+        root.left = Node(value)
+        return root
+    
+    q = deque()
+    q.append(root)
+    
+    while q:
+        temp = q.popleft()
+        if temp.left is None:
+            temp.left = Node(value)
+            break
+        else:
+            q.append(temp.left)
+        
+        if temp.right is None:
+            temp.right = Node(value)
+            break
+        else:
+            q.append(temp.right)
+        
+    return root
 
-# root.left = None
-# root.right = None
-# root = None
+# root = Node(10)
+# root.left = Node(12)
+# root.left.left = Node(20)
+# root.left.right = Node(24)
+# root.left.left.left = Node(38)
+# root.left.left.right = Node(40)
+
+# root.right = Node(18)
+# root.right.left = Node(28)
+# root.right.right = Node(32)
+# root.right.left.left = Node(45)
+# root.right.right.right = Node(50)
+
+root = None
+
+nodes = [2,4,6,12,18,24,38,54,38,40,90,45,94,99,120]
+
+for node in nodes:
+    root = insert(root,node)
 
 print('in order:')
 inorder(root)
